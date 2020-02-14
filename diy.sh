35272c3cb95879443728c92f9a23cc2176b4dd5f
chenxiccc <chenxiccc@gmail.com>
2020-02-15 00:54:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,6 +11,10 @@ sed -i 's/192.168.1.1/192.168.31.200/g' package/base-files/files/bin/config_gene
 wget -O package/network/services/dnsmasq/files/dhcp.conf https://raw.githubusercontent.com/chenxiccc/Customfiles/master/newifi3dhcp
 wget -O package/base-files/files/etc/hosts https://raw.githubusercontent.com/chenxiccc/Customfiles/master/hosts
 wget -O package/network/config/firewall/files/firewall.config https://raw.githubusercontent.com/chenxiccc/Customfiles/master/newifi3firewall
+wget -O feeds/lienol/lienol/luci-app-passwall/root/etc/config/passwall_rule/blacklist_host https://raw.githubusercontent.com/chenxiccc/Customfiles/master/blacklist_host
+wget -O feeds/lienol/lienol/luci-app-passwall/root/etc/config/passwall_rule/whitelist_host https://raw.githubusercontent.com/chenxiccc/Customfiles/master/whitelist_host
+wget -O feeds/lienol/lienol/luci-app-passwall/root/etc/config/passwall_rule/blacklist_ip https://raw.githubusercontent.com/chenxiccc/Customfiles/master/blacklist_ip
+wget -O feeds/lienol/lienol/luci-app-passwall/root/etc/config/passwall_rule/whitelist_ip https://raw.githubusercontent.com/chenxiccc/Customfiles/master/whitelist_ip
 #sed -i '1i\127.0.0.1 api.ad.xiaomi.com' package/base-files/files/etc/hosts
 #sed -i '1i\127.0.0.1 sdkconfig.ad.xiaomi.com' package/base-files/files/etc/hosts
 #sed -i '1i\127.0.0.1 ad.mi.com' package/base-files/files/etc/hosts
