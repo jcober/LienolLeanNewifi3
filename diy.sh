bfc599510ce6f905340292e2a869c2b2ff716a28
chenxiccc <chenxiccc@gmail.com>
2020-02-14 21:56:36 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,7 +8,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.31.200/g' package/base-files/files/bin/config_generate
-wget -O package/network/services/dnsmasq/files/dhcp.conf https://raw.githubusercontent.com/chenxiccc/Customfiles/master/newwifi3dhcp
+wget -O package/network/services/dnsmasq/files/dhcp.conf https://raw.githubusercontent.com/chenxiccc/Customfiles/master/newifi3dhcp
 wget -O package/base-files/files/etc/hosts https://raw.githubusercontent.com/chenxiccc/Customfiles/master/hosts
 wget -O package/network/config/firewall/files/firewall.config https://raw.githubusercontent.com/chenxiccc/Customfiles/master/newifi3firewall
 #sed -i '1i\127.0.0.1 api.ad.xiaomi.com' package/base-files/files/etc/hosts
