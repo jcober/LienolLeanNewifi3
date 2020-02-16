96f03fd3fb4952f3fd1bc3427ab1b345b595aab4
chenxiccc <chenxiccc@gmail.com>
2020-02-16 12:50:48 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,9 +8,13 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.31.200/g' package/base-files/files/bin/config_generate
-wget -O package/network/services/dnsmasq/files/dhcp.conf https://raw.githubusercontent.com/chenxiccc/Customfiles/master/newifi3/dhcp
-wget -O package/base-files/files/etc/hosts https://raw.githubusercontent.com/chenxiccc/Customfiles/master/hosts
-wget -O package/network/config/firewall/files/firewall.config https://raw.githubusercontent.com/chenxiccc/Customfiles/master/newifi3/firewall
+wget -O tmp/hosts https://raw.githubusercontent.com/chenxiccc/Customfiles/master/hosts
+wget -O tmp/dhcp.tmp https://raw.githubusercontent.com/chenxiccc/Customfiles/master/newifi3/dhcp
+cat tmp/dhcp.tmp >> package/network/services/dnsmasq/files/dhcp.conf
+rm tmp/dhcp.tmp
+wget -O tmp/firewall.tmp https://raw.githubusercontent.com/chenxiccc/Customfiles/master/newifi3/firewall
+cat tmp/firewall.tmp >> package/network/config/firewall/files/firewall.config
+rm tmp/firewall.tmp
 wget -O feeds/lienol/lienol/luci-app-passwall/root/etc/config/passwall_rule/blacklist_host https://raw.githubusercontent.com/chenxiccc/Customfiles/master/pw/blacklist_host
 wget -O feeds/lienol/lienol/luci-app-passwall/root/etc/config/passwall_rule/whitelist_host https://raw.githubusercontent.com/chenxiccc/Customfiles/master/pw/whitelist_host
 wget -O feeds/lienol/lienol/luci-app-passwall/root/etc/config/passwall_rule/blacklist_ip https://raw.githubusercontent.com/chenxiccc/Customfiles/master/pw/blacklist_ip
